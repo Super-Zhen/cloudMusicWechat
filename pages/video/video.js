7d34b83bb2141e6462ff9dc600b011e7309f9973
Super-Zhen <zhenchaoqun@163.com>
2019-08-19 23:35:14 +0800

    视频滑动

--- pages/video/video.js
+++ pages/video/video.js
@@ -7,7 +7,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-    videoList:''
+    videoList: '', 
+    winHeight:''
   },
   /**
    * 获取视频列表
@@ -26,9 +27,29 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var that = this;
+    //  高度自适应
+    wx.getSystemInfo({
+      success: function (res) {
+        var clientHeight = res.windowHeight,
+          clientWidth = res.windowWidth,
+          rpxR = 750 / clientWidth;
+        var calc = clientHeight * rpxR ;
+        console.log(calc)
+        that.setData({
+          winHeight: calc,
+          // currentTab:'1'
+        });
+      }
+    });
     this.getVideoList()
   },
-
+  /**
+   * 滑动到底部
+   */
+  lower(){
+    console.log(1)
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
